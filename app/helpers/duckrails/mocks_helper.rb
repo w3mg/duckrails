fa5c1b5ef521a9f6ce5b1c20e42345ff4666da3e
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-09 20:14:14 +0200

    Add convenient methods for retrieving stuff for the mock form.

--- app/helpers/duckrails/mocks_helper.rb
+++ app/helpers/duckrails/mocks_helper.rb
@@ -1,4 +1,16 @@
 module Duckrails
   module MocksHelper
+    def available_mime_types
+      Mime::EXTENSION_LOOKUP.map{ |a| a[1].to_s }.uniq
+    end
+
+    def available_body_types
+      [ [t(Duckrails::Mock::BODY_TYPE_EMBEDDED_RUBY), Duckrails::Mock::BODY_TYPE_EMBEDDED_RUBY],
+        [t(Duckrails::Mock::BODY_TYPE_STATIC), Duckrails::Mock::BODY_TYPE_STATIC] ]
+    end
+
+    def available_http_methods
+      [:get, :post, :put, :patch, :delete, :options]
+    end
   end
 end

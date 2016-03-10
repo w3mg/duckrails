fbd3f928990f309ba7e5bfeb07a24cd410927cad
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-11 00:04:09 +0200

    Added a route validator.

---
+++ lib/route_validator.rb
@@ -0,0 +1,24 @@
+class RouteValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return if record.method.blank?
+
+    valid = begin
+      # Parse path to catch bad URIs
+      URI.parse value
+      # Try to see if there's already a route matching the specific method and route path
+      route = Rails.application.routes.recognize_path(value, method: record.method)
+      # If the route exists, then it should belong to the same record in order to be valid
+      # or else we would allow many mocks to have the same route
+      valid = route[:controller] == 'duckrails/mocks' && route[:action] == 'mockify' && route[:duckrails_mock_id] == record.id
+    rescue URI::InvalidURIError => exception
+      # Bad URI
+      record.errors[attribute] << (options[:message] || "is not a valid route")
+    rescue ActionController::RoutingError => exception
+      # FIXME: check if this exception is raised in other cases except when the route doesn't exist
+      # Route doesn't exist, you may proceed
+      valid = true
+    end
+
+    record.errors[attribute] << 'already in use' unless valid
+  end
+end

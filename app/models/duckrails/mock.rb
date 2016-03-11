67f1618cf82274a520c6e2cd9eadc9279828d208
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-11 22:55:27 +0200

    Updated the mock model to comply with the db updates.

--- app/models/duckrails/mock.rb
+++ app/models/duckrails/mock.rb
@@ -1,26 +1,30 @@
 class Duckrails::Mock < ActiveRecord::Base
-  BODY_TYPE_EMBEDDED_RUBY = 'embedded_ruby'
-  BODY_TYPE_STATIC = 'static'
+  SCRIPT_TYPE_EMBEDDED_RUBY = 'embedded_ruby'
+  SCRIPT_TYPE_STATIC = 'static'
 
-  has_many :headers
+  has_many :headers, dependent: :destroy
   accepts_nested_attributes_for :headers, allow_destroy: true, reject_if: :all_blank
-
-  has_one :body
 
   validates :status, presence: true
   validates :method, presence: true
-  validates :route_path, presence: true, route: true, uniqueness: true
+  validates :route_path, presence: true, route: true, uniqueness: { scope: :method }
   validates :name, presence: true, uniqueness: true
-  validates :body_type, inclusion: { in: [BODY_TYPE_STATIC,
-                                          BODY_TYPE_EMBEDDED_RUBY],
+  validates :body_type, inclusion: { in: [SCRIPT_TYPE_STATIC,
+                                          SCRIPT_TYPE_EMBEDDED_RUBY],
                                      allow_blank: true },
-                        presence: true
+                        presence: { unless: 'body_content.blank?' }
+  validates :body_content, presence: { unless: 'body_type.blank?' }
+  validates :script_type, inclusion: { in: [SCRIPT_TYPE_STATIC,
+                                            SCRIPT_TYPE_EMBEDDED_RUBY],
+                                     allow_blank: true },
+                          presence: { unless: 'script.blank?' }
+  validates :script, presence: { unless: 'script_type.blank?' }
 
   after_save :register
   after_destroy :unregister
 
   def dynamic?
-    body_type != BODY_TYPE_STATIC
+    body_type != SCRIPT_TYPE_STATIC
   end
 
   def register

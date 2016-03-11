de2ea1b3136130254c559fa02e5406f621ce0a67
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-11 22:55:41 +0200

    Delete headers on mock deletion.

--- db/migrate/20160304204007_create_duckrails_headers.rb
+++ db/migrate/20160304204007_create_duckrails_headers.rb
@@ -3,7 +3,7 @@ class CreateDuckrailsHeaders < ActiveRecord::Migration
     create_table :headers do |t|
       t.string :name, null: false
       t.string :value, null: false
-      t.references :mock, index: true, foreign_key: true
+      t.references :mock, index: true, foreign_key: { on_delete: :cascade }
 
       t.timestamps null: false
     end

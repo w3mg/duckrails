26d9da0096f656d6b5b44dbc7359373ef797d684
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-11 22:56:32 +0200

    Don’t register mocks unless the table has already been created (first run error).

--- config/initializers/register_mocks.rb
+++ config/initializers/register_mocks.rb
@@ -1,3 +1,4 @@
 require 'duckrails/router'
 
-Duckrails::Router.register_current_mocks
+
+Duckrails::Router.register_current_mocks if ActiveRecord::Base.connection.table_exists? Duckrails::Mock.table_name

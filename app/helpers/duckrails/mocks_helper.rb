d637a7f4be40d4d61a3efb2137d9c300db818863
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-11 22:58:03 +0200

    Improved naming in helper method for available script types.

--- app/helpers/duckrails/mocks_helper.rb
+++ app/helpers/duckrails/mocks_helper.rb
@@ -4,9 +4,9 @@ module Duckrails
       Mime::EXTENSION_LOOKUP.map{ |a| a[1].to_s }.uniq
     end
 
-    def available_body_types
-      [ [t(Duckrails::Mock::BODY_TYPE_EMBEDDED_RUBY), Duckrails::Mock::BODY_TYPE_EMBEDDED_RUBY],
-        [t(Duckrails::Mock::BODY_TYPE_STATIC), Duckrails::Mock::BODY_TYPE_STATIC] ]
+    def available_script_types
+      [ [t(Duckrails::Mock::SCRIPT_TYPE_EMBEDDED_RUBY), Duckrails::Mock::SCRIPT_TYPE_EMBEDDED_RUBY],
+        [t(Duckrails::Mock::SCRIPT_TYPE_STATIC), Duckrails::Mock::SCRIPT_TYPE_STATIC] ]
     end
 
     def available_http_methods

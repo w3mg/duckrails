c060e03bd26e68840fc792cac9b673c891cb9713
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-21 23:31:50 +0200

    Uniqueness of name attribute of a mock is now case insensitive.

--- app/models/duckrails/mock.rb
+++ app/models/duckrails/mock.rb
@@ -9,7 +9,7 @@ class Duckrails::Mock < ActiveRecord::Base
   validates :request_method, presence: true
   validates :content_type, presence: true
   validates :route_path, presence: true, route: true, uniqueness: { scope: :request_method }
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :body_type, inclusion: { in: [SCRIPT_TYPE_STATIC,
                                           SCRIPT_TYPE_EMBEDDED_RUBY],
                                      allow_blank: true },

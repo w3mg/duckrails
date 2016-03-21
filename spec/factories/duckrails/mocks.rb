a1631cad50ebc4861d24b0624495c70caf51f73d
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-21 23:22:47 +0200

    Configure the RSpec world.

---
+++ spec/factories/duckrails/mocks.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :mock, class: Duckrails::Mock do
+    status '200'
+    request_method  'get'
+    content_type 'application/json'
+    route_path '/mocks/:id'
+    name 'Default mock'
+  end
+end

4a973a1fd254397ad7402f6626a6057551760749
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-22 21:52:56 +0200

    Add specs for index action of home controller.

--- spec/controllers/duckrails/home_controller_spec.rb
+++ spec/controllers/duckrails/home_controller_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe Duckrails::HomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
+
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
     end
   end
-
 end

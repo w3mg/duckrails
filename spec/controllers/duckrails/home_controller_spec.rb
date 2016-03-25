df55364d44639d6d8abfb57af05a5f0d73901cd8
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-25 13:35:12 +0200

    Cleanup.

--- spec/controllers/duckrails/home_controller_spec.rb
+++ spec/controllers/duckrails/home_controller_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Duckrails::HomeController, type: :controller do
+module Duckrails
+  RSpec.describe HomeController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
+    describe "GET #index" do
+      it "returns http success" do
+        get :index
 
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:index)
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:index)
+      end
     end
   end
 end

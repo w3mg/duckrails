bb14c5497059c102cbe4ccb5d6d2dd59352fac94
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-03-25 15:32:08 +0200

    Add view specs for application’s index.

--- spec/views/duckrails/home/index.html.erb_spec.rb
+++ spec/views/duckrails/home/index.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'duckrails/home/index.html.erb', type: :view do
+  let(:title) { t('Home') }
+
+  before do
+    render
+  end
+
+  subject { rendered }
+
+  context 'content' do
+    it { should have_css ".welcome img[src='#{view.asset_path('duckrails.png')}']" }
+    it { should have_css '.welcome .home-welcome h1', text: t(:mock_the_universe) }
+    it { should have_css '.welcome .home-welcome .page-guide', text: t(:welcome_message) }
+
+    it { should have_css "a.button[href='#{view.duckrails_mocks_path}']", text: t(:view_all_mocks) }
+    it { should have_css "a.button[href='#{view.new_duckrails_mock_path}']", text: t(:create_new_mock) }
+  end
 end

743d8f36f2725c698918026f84827be5bdcbd177
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2016-04-10 23:29:27 +0300

    Don’t register mocks if there are pending migrations.

--- config/initializers/register_mocks.rb
+++ config/initializers/register_mocks.rb
@@ -1,3 +1,4 @@
 require 'duckrails/router'
 
-Duckrails::Router.register_current_mocks if ActiveRecord::Base.connection.table_exists? Duckrails::Mock.table_name
+# Don't register mocks if there are pending migrations
+Duckrails::Router.register_current_mocks unless ActiveRecord::Migrator.needs_migration?

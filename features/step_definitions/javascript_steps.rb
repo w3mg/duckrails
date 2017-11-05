334471b6195bfd3d61bf292b93d2e38da590bca2
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2017-11-05 03:49:25 +0200

    Remove unused confirmation steps.

--- features/step_definitions/javascript_steps.rb
+++ features/step_definitions/javascript_steps.rb
@@ -2,14 +2,4 @@ Then /^I should see a javascript confirmation with text '(.*)'$/ do |text|
   expect(page.driver.browser.switch_to.alert.text).to eq text
 end
 
-When /^I accept the javascript confirmation$/ do
-  page.driver.browser.switch_to.alert.accept
-  # FIXME: we sleep here to allow the rails url helpers to be successfully reloaded with the new routes
-  sleep 2
-end
-
-When /^I dismiss the javascript confirmation$/ do
-  page.driver.browser.switch_to.alert.dismiss
-  # FIXME: we sleep here to allow the rails url helpers to be successfully reloaded with the new routes
-  sleep 2
 end

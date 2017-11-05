f73132c8f87c0ca29531a5c78f72c58dd13d6241
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2017-11-06 00:13:08 +0200

    Bump version to 2.0.0

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Duckrails
   class Application < Rails::Application
-    VERSION = '1.2.1'
+    VERSION = '2.0.0'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
